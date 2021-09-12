7a34e1241a7e55feb3bd5252e2cbb80c925a2cf4
Frederic Vogels <frederic.vogels@ucll.be>
2021-09-12 18:01:13 +0200

    project file update

--- wpf/3d-studio/3d-studio/Configuration.cs
+++ wpf/3d-studio/3d-studio/Configuration.cs
@@ -10,7 +10,7 @@ namespace WifViewer
     public static class Configuration
     {
         // Backslashes allowed, don't bother changing them into forward slashes
-        public static string RAYTRACER_PATH = @"E:\repos\ucll\shared\scripts\raytracer.exe";
+        public static string RAYTRACER_PATH = @"G:\repos\ucll\3dcg\raytracer\raytracer\x64\Release\raytracer.exe";
 
         // If the script has a filename, it will be saved upon rendering.
         public static bool AUTO_SAVE = true;
